56f10d62d8ec2c7abed755f9864902ca05b750c3
zhansvltan <assemzhar@icloud.com>
2026-05-22 00:51:33 +0500

    pos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
       <body className="relative size-full overflow-hidden bg-[url('/mobile-bg.webp')] bg-cover bg-center bg-no-repeat font-sans text-foreground md:bg-[url('/desktop-bg.webp')] md:bg-[length:100%_auto] md:bg-top">
         {children}
         <img
-          className="pointer-events-none hidden absolute z-30 right-[0] bottom-[0] select-none md:w-[55%] md:block"
+          className="pointer-events-none hidden absolute z-30 right-0 bottom-0 select-none md:w-[55%] md:block"
           src="/right-corner-desktop.webp"
           alt=""
         />
@@ -69,17 +69,17 @@ export default function RootLayout({
           alt=""
         />
         <img
-          className="pointer-events-none block absolute z-30 left-0 top-0 w-[95%] select-none md:w-[40%] md:hidden"
+          className="pointer-events-none block absolute z-30 left-0 -top-6 w-[95%] select-none md:w-[40%] md:hidden"
           src="/left-corner-mobile.webp"
           alt=""
         />
         <img
-          className="pointer-events-none block absolute z-30 right-[32%] bottom-0 w-[60%] select-none md:w-[40%] md:hidden"
+          className="pointer-events-none block absolute z-30 right-[32%] -bottom-6 w-[60%] select-none md:w-[40%] md:hidden"
           src="/right-corner-mobile-pl.webp"
           alt=""
         /> 
         <img
-          className="pointer-events-none block absolute z-30 right-0 bottom-0 w-[48%] select-none md:w-[40%] md:hidden"
+          className="pointer-events-none block absolute z-30 right-0 -bottom-6 w-[48%] select-none md:w-[40%] md:hidden"
           src="/right-corner-mobile-pr.webp"
           alt=""
         />
